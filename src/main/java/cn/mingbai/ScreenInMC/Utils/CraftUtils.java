11f48a988169c00fdb325b3cf876b42dda74b507
GZY明明白白 <2785726662@qq.com>
2023-01-03 15:46:02 +0800

    Update

--- src/main/java/cn/mingbai/ScreenInMC/Utils/CraftUtils.java
+++ src/main/java/cn/mingbai/ScreenInMC/Utils/CraftUtils.java
@@ -3,7 +3,7 @@ package cn.mingbai.ScreenInMC.Utils;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerPlayerConnection;
-import org.bukkit.craftbukkit.v1_19_R2.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class CraftUtils {
